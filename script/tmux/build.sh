a68857b3ae1ae8252915d729aebe348a9f7b81c6
David Callu <callu.david@gmail.com>
2023-05-17 00:03:18 +0200

    feat(tmux): add tmux

---
+++ script/tmux/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -x
+
+sudo yum -y install \
+     automake \
+     byacc \
+     libevent-devel \
+     ncurse-devel
+     
+
+TMUX_VERSION=3.3a
+TMUX_ARCHIVE=${TMUX_VERSION}.tar.gz
+TMUX_SRC_DIR=tmux-${TMUX_VERSION}
+TMUX_INSTALL_DIR=/tmp/install/${TMUX_SRC_DIR}
+TMUX_FINAL_ARCHIVE_PATH=/tmp/install/${TMUX_SRC_DIR}.tar.xz
+
+curl -L -O https://github.com/tmux/tmux/archive/refs/tags/${TMUX_ARCHIVE}
+tar xvf ${TMUX_ARCHIVE}
+cd ${TMUX_SRC_DIR}
+./autogen.sh
+./configure --prefix ${TMUX_INSTALL_DIR}
+make ${nproc}
+make install
+
+TMUX_FINAL_URCHIVE_PATH
+
+
+tar Jcvf ${TMUX_FINAL_ARCHIVE_PATH} \
+    -C $(dirname ${TMUX_INSTALL_DIR}) \
+    $(basename ${TMUX_INSTALL_DIR})
