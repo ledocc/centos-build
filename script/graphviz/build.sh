6e43ce7f9fdf3fe284a2d7f7f3686ac6e8fc913b
David Callu <callu.david@gmail.com>
2023-05-29 23:19:29 +0200

    feat(graphviz): add graphviz

---
+++ script/graphviz/build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+
+sudo yum -y install \
+    librsvg2-devel \
+    cairo-devel \
+    pango-devel \
+    poppler-devel \
+    libwebp-devel
+    
+
+
+GRAPHVIZ_VERSION=8.0.5
+GRAPHVIZ_SRC_DIR=graphviz-${GRAPHVIZ_VERSION}
+GRAPHVIZ_ARCHIVE=${GRAPHVIZ_SRC_DIR}.tar.xz
+GRAPHVIZ_INSTALL_DIR=/tmp/install/${GRAPHVIZ_SRC_DIR}-$(uname)-$(uname -p)
+GRAPHVIZ_FINAL_ARCHIVE_PATH=/tmp/install/${GRAPHVIZ_SRC_DIR}.tar.xz
+
+
+curl -L -O https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/${GRAPHVIZ_VERSION}/${GRAPHVIZ_ARCHIVE}
+tar xvf ${GRAPHVIZ_ARCHIVE}
+cd ${GRAPHVIZ_SRC_DIR}
+./configure --prefix ${GRAPHVIZ_INSTALL_DIR}
+make ${nproc}
+make install
+
+tar Jcvf ${GRAPHVIZ_FINAL_ARCHIVE_PATH} \
+    -C $(dirname ${GRAPHVIZ_INSTALL_DIR}) \
+    $(basename ${GRAPHVIZ_INSTALL_DIR})
