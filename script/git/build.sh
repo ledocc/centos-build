1b725e6f520b5a262c019f0b3f4e6f0d6479161a
David Callu <callu.david@gmail.com>
2023-06-02 10:01:51 +0200

    refacto(git): use common script

--- script/git/build.sh
+++ script/git/build.sh
@@ -4,39 +4,28 @@ set -e
 set -x
 
 THIS_SCRIPT_DIR=$(dirname $(realpath $0))
+source ${THIS_SCRIPT_DIR}/../common.sh
 
-. /opt/rh/devtoolset-10/enable
+
 
 
 sudo yum -y install \
      asciidoc \
+     tcl-devel \
+     tk-devel \
      xmlto
 
+init_work_dir git 2.41.0
 
-GIT_VERSION=2.40.1
 
-GIT_BUILD_DIR=/tmp/git-${GIT_VERSION}-build
-mkdir -p ${GIT_BUILD_DIR}
-pushd ${GIT_BUILD_DIR}
-
-GIT_DIR_NAME=git-${GIT_VERSION}
+SRC_DIR_NAME=git-${VERSION}
 GIT_ARCHIVE=${GIT_DIR_NAME}.tar.gz
-curl -L -O https://www.kernel.org/pub/software/scm/git/${GIT_ARCHIVE}
-tar xvf ${GIT_ARCHIVE}
 
-GIT_INSTALL_DIR=${GIT_BUILD_DIR}/install/git-${GIT_VERSION}-$(uname)-$(uname -p)
-cd ${GIT_DIR_NAME}
-./configure --prefix=${GIT_INSTALL_DIR}
+download_and_extract https://www.kernel.org/pub/software/scm/git/${SRC_DIR_NAME}.tar.gz
+autotool_build ${WORK_DIR}/${SRC_DIR_NAME}
 make -j $(nproc) all man
-
 make install install-man
-cp ${THIS_SCRIPT_DIR}/.profile_git ${GIT_INSTALL_DIR}
 
-GIT_FINAL_ARCHIVE_PATH=${GIT_INSTALL_DIR}.txz
-tar Jcvf ${GIT_FINAL_ARCHIVE_PATH} \
-    -C $(dirname ${GIT_INSTALL_DIR}) \
-    $(basename ${GIT_INSTALL_DIR})
+cp ${THIS_SCRIPT_DIR}/.profile_git ${INSTALL_DIR}
 
-cp ${GIT_FINAL_ARCHIVE_PATH} /tmp/install
-
-popd
+make_archive
